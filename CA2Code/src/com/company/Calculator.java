881fc1728d65bd7dcf9fd5421887069f61b5d639
danielmartellini <danielbmartellini@gmail.com>
2020-11-17 01:36:55 +0000

    fixing code

--- CA2Code/src/com/company/Calculator.java
+++ CA2Code/src/com/company/Calculator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CA2Code.src.com.company;
+package CA2Code.src.com.company.Calculator;
 
 import java.util.Scanner;
 
